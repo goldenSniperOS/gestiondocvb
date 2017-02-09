d685c007cd26cd329ed663ff4782fea2ef23f535
MazFreeLancer <manuelzenareque@gmail.com>
2017-02-09 18:43:17 -0500

    Reportando Ando Ando x 2 :v:

---
+++ Seguridad/frmReporteAreaPerido.vb
@@ -0,0 +1,13 @@
+﻿Public Class frmReporteAreaPerido
+    Property Area As String
+    Property FechaIni As Date
+    Property FechaFin As Date
+
+    Private Sub frmReporteAreaPerido_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim objRep As New rptGastoMovilidadAreaPeriodo
+        objRep.SetParameterValue("@FechaIni", FechaIni)
+        objRep.SetParameterValue("@FechaFin", FechaFin)
+
+        CrystalReportViewer1.ReportSource = objRep
+    End Sub
+End Class
