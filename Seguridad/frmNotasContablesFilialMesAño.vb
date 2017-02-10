b98bbec5fabf0f325249c1c5fc3e49161b67d160
MazFreeLancer <manuelzenareque@gmail.com>
2017-02-09 21:52:23 -0500

    Reportando ando ando x4 :v:

---
+++ Seguridad/frmNotasContablesFilialMesAño.vb
@@ -0,0 +1,8 @@
+﻿Public Class frmNotasContablesFilialMesAño
+
+    Private Sub btnReporte_Click(sender As Object, e As EventArgs) Handles btnReporte.Click
+        Dim objfrm As New frmReporteNotaFilialMesAnio
+        objfrm.Anio = CInt(txtAnio.Text)
+        objfrm.Show()
+    End Sub
+End Class
