284624b78c7a5e63f22ba7f5155bf1e9be34de64
MazFreeLancer <manuelzenareque@gmail.com>
2017-02-09 19:49:07 -0500

    Reportando ando ando x3 :v:

---
+++ Seguridad/frmNotaPorUsuarioPorAnio.vb
@@ -0,0 +1,8 @@
+﻿Public Class frmNotaPorUsuarioPorAnio
+
+    Private Sub btnReporte_Click(sender As Object, e As EventArgs) Handles btnReporte.Click
+        Dim objfrm As New frmReporteNotaUsuarioAnio
+        objfrm.Anio = CInt(txtAnio.Text)
+        objfrm.Show()
+    End Sub
+End Class
---
+++ Seguridad/frmReporteNotaUsuarioAnio.vb
@@ -0,0 +1,10 @@
+﻿Public Class frmReporteNotaUsuarioAnio
+    Property Anio As Integer
+
+    Private Sub frmReporteNotaUsuarioAnio_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim objRep As New rptReporteNotaUsuarioAnio
+        objRep.SetParameterValue("@Año", Anio)
+
+        CrystalReportViewer1.ReportSource = objRep
+    End Sub
+End Class
