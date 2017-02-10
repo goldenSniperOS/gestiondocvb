82c99aa37a4ae4ab9e2cd36f8a7701da6c6e077d
MazFreeLancer <manuelzenareque@gmail.com>
2017-02-09 22:53:24 -0500

    Reportando ando ando x5 :v: Con este Queda :v:

---
+++ Seguridad/frmPapeletasAreaMesAnio.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPapeletasAreaMesAnio
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnReporte = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtAnio = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnReporte
+        '
+        Me.btnReporte.Location = New System.Drawing.Point(251, 69)
+        Me.btnReporte.Name = "btnReporte"
+        Me.btnReporte.Size = New System.Drawing.Size(75, 23)
+        Me.btnReporte.TabIndex = 5
+        Me.btnReporte.Text = "REPORTE"
+        Me.btnReporte.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtAnio)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(196, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(189, 51)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Seleccion Año"
+        '
+        'txtAnio
+        '
+        Me.txtAnio.Location = New System.Drawing.Point(73, 23)
+        Me.txtAnio.Name = "txtAnio"
+        Me.txtAnio.Size = New System.Drawing.Size(100, 20)
+        Me.txtAnio.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(38, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Año:"
+        '
+        'frmPapeletasAreaMesAnio
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(605, 127)
+        Me.Controls.Add(Me.btnReporte)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "frmPapeletasAreaMesAnio"
+        Me.Text = "frmPapeletasAreaMesAnio"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnReporte As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtAnio As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+End Class
---
+++ Seguridad/frmReportePapeletaAreaMesAnio.vb
@@ -0,0 +1,11 @@
+﻿Public Class frmReportePapeletaAreaMesAnio
+    Property Anio As Integer
+
+
+    Private Sub frmReportePapeletaAreaMesAnio_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim objRep As New rptReportePapeletaAreaMesAnio
+        objRep.SetParameterValue("@Año", Anio)
+
+        CrystalReportViewer1.ReportSource = objRep
+    End Sub
+End Class
